fe29b41f33d6553b6009c327e7c783440d82f059
GranKaen <63749175+GranKaen@users.noreply.github.com>
2020-06-05 00:47:46 +0400

    Add files via upload

---
+++ Taxiappdavaleba/Taxiappdavaleba/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Taxiappdavaleba
+//
+//  Created by konstantine gudushauri on 6/4/20.
+//  Copyright © 2020 konstantine gudushauri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        navigationController?.isNavigationBarHidden = true    }
+
+
+}
+
