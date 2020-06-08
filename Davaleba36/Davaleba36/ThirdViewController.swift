28509b4a6a3df0f5ebaf3003945074c306f053f5
GranKaen <63749175+GranKaen@users.noreply.github.com>
2020-06-08 17:29:10 +0400

    Add files via upload

---
+++ Davaleba36/Davaleba36/ThirdViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdViewController.swift
+//  Davaleba36
+//
+//  Created by konstantine gudushauri on 6/8/20.
+//  Copyright © 2020 konstantine gudushauri. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.isNavigationBarHidden = true
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Davaleba36/Davaleba36/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Davaleba36
+//
+//  Created by konstantine gudushauri on 6/8/20.
+//  Copyright © 2020 konstantine gudushauri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.isNavigationBarHidden = true
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
